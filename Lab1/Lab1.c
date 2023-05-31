17be21f8956157cf9b712febf309f45ba8ffd1f2
Agustin Haristoy <agustin.haristoy@usach.cl>
2023-05-31 02:01:53 -0400

    Estructura Basica del Lab

---
+++ Lab1/Lab1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.c"
+#include <sys/wait.h>
+#include <unistd.h>
+
---
+++ Lab1/funciones.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+
+
